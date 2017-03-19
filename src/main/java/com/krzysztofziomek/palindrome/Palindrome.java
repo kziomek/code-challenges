f7d72f6f9cf2529c9af06bb369e1db1fa7b3f45e
Krzysztof Ziomek <kziomek@onet.eu>
2017-03-19 12:10:04 +0000

    Algorithm : Palindrome - challenge description

--- src/main/java/com/krzysztofziomek/palindrome/Palindrome.java
+++ src/main/java/com/krzysztofziomek/palindrome/Palindrome.java
@@ -3,6 +3,10 @@ package com.krzysztofziomek.palindrome;
 /**
  * @author Krzysztof Ziomek
  * @since 19/03/2017.
+ *
+ * Write an efficient algorithm to check if a string is a palindrome. A string is a palindrome if the string matches the reverse of string.
+ * Example: 1221 is a palindrome but not 1121.
+ *
  */
 public class Palindrome {
 
