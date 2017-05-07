eb5b6220532cdf14edf2049ff7879843ca7bdd7d
Krzysztof Ziomek <kziomek@onet.eu>
2017-05-07 13:51:35 +0100

    MaxCounters performance O(n^2) -> O(n)

--- src/main/java/com/krzysztofziomek/codility/MaxCounters.java
+++ src/main/java/com/krzysztofziomek/codility/MaxCounters.java
@@ -9,20 +9,27 @@ public class MaxCounters {
     public int[] solution(int N, int[] A) {
         int[] counters = new int[N];
         int max = 0;
+        int activeMax = 0;
 
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] == N + 1) {
-                for (int j = 0; j < N; j++) {
-                    counters[j] = max;
-                }
+        for (int i=0; i<A.length; i++){
+            if (A[i]==N+1){
+                activeMax = max;
             } else {
-                counters[A[i] - 1]++;
-                if (counters[A[i] - 1] > max) {
-                    max = counters[A[i] - 1];
+                if (counters[A[i]-1] < activeMax){
+                    counters[A[i]-1] = activeMax;
+                }
+                counters[A[i]-1]++;
+                if (counters[A[i]-1]>max){
+                    max = counters[A[i]-1];
                 }
             }
         }
 
+        for (int j=0;j<counters.length;j++){
+            if (counters[j] < activeMax){
+                counters[j] = activeMax;
+            }
+        }
         return counters;
     }
 
