e81462cead0ba61f5d89b9245f47388361998a8f
Krzysztof Ziomek <kziomek@onet.eu>
2021-01-23 20:32:46 +0000

    Longest Common Subsequence - recursive implementation

--- src/main/java/com/krzysztofziomek/dynamic/LCS.java
+++ src/main/java/com/krzysztofziomek/dynamic/LCS.java
@@ -6,28 +6,40 @@ package com.krzysztofziomek.dynamic;
  */
 public class LCS {
 
+    // recursive
+    public int lcs(char[] x, char[] y, int m, int n) {
+        if (m < 0 || n < 0) {
+            return 0;
+        }
+        if (x[m] == y[n]) {
+            return 1 + lcs(x, y, m - 1, n - 1);
+        } else {
+            return Math.max(lcs(x, y, m - 1, n), lcs(x, y, m, n - 1));
+        }
+    }
+
+    // dynamic programming
     public int getLongestCommonSubsequence(String X, String Y) {
         int m = X.length();
         int n = Y.length();
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-                }
+        int[][] dp = new int[n + 1][m + 1];
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (X.charAt(j - 1) == Y.charAt(i - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
-        return dp[m][n];
+        return dp[n][m];
     }
 
     public static void main(String[] args) {
         String X = "ABCBDAB";
         String Y = "BDCABA";
         System.out.println(new LCS().getLongestCommonSubsequence(X, Y));
+        System.out.println(new LCS().lcs(X.toCharArray(), Y.toCharArray(), X.length() - 1, Y.length() - 1));
     }
 }
