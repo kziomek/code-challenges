d1a2781162fbcebb266bc722c3cf91565b72dd33
Krzysztof Ziomek <kziomek@onet.eu>
2021-01-23 16:38:44 +0000

    DinnerParty update

--- src/main/java/com/krzysztofziomek/recursive/DinnerParty.java
+++ src/main/java/com/krzysztofziomek/recursive/DinnerParty.java
@@ -11,41 +11,54 @@ import java.util.Set;
  */
 public class DinnerParty {
 
-    public List<Set<Integer>> findDinnerParties(int[] friends, int tableSize) {
-
-        List<Set<Integer>> groups = new ArrayList<>();
-        Set<Integer> group = new HashSet<>();
-        combineFriends(friends, tableSize, 0, groups, group);
+    public List<Set<Character>> findDinnerParties(char[] friends, int tableSize) {
+        List<Set<Character>> groups = new ArrayList<>();
+        Set<Character> group = new HashSet<>();
+        combineFriends(friends, tableSize, 0,   groups, group);
         return groups;
 
     }
 
-    private void combineFriends(int[] friends, int tableSize, int pos, List<Set<Integer>> groups, Set<Integer> group) {
-        // stop
+    private void combineFriends(char[] friends, int tableSize, int pos, List<Set<Character>> groups, Set<Character> group) {
         if (group.size() == tableSize) {
-            groups.add(group);
-        } else if (pos < friends.length) {
-            //leave
-            combineFriends(friends, tableSize, pos + 1, groups, group);
+            groups.add(new HashSet<>(group));
+            return;
+        }
 
-            //take
-            group = new HashSet<>(group);
-            group.add(friends[pos]);
-            combineFriends(friends, tableSize, pos + 1, groups, group);
+        for (int i = pos; i<friends.length; i++) {
+            group.add(friends[i]);
+            combineFriends(friends, tableSize, i + 1, groups, group);
+            group.remove(friends[i]);
         }
     }
+
+    /*
+      https://www.youtube.com/watch?v=3teHU4n-czU&ab_channel=JacksonGabbard
+    */
+//    private void combineFriends(int[] friends, int tableSize, int pos, List<Set<Integer>> groups, Set<Integer> group) {
+//        // stop
+//        if (group.size() == tableSize) {
+//            groups.add(group);
+//        } else if (pos < friends.length) {
+//            //leave
+//            combineFriends(friends, tableSize, pos + 1, groups, group);
+//
+//            //take
+//            group = new HashSet<>(group);
+//            group.add(friends[pos]);
+//            combineFriends(friends, tableSize, pos + 1, groups, group);
+//        }
+//    }
 
 
     public static void main(String[] args) {
-        int[] friends = {1, 2, 3, 4};
+        char[] friends = {'A', 'B', 'C', 'D', 'E' , 'F'};
         int tableSize = 3;
 
-        List<Set<Integer>> dinnerParties = new DinnerParty().findDinnerParties(friends, tableSize);
+        List<Set<Character>> dinnerParties = new DinnerParty().findDinnerParties(friends, tableSize);
 
-        for (Set<Integer> dinnerParty : dinnerParties) {
+        for (Set<Character> dinnerParty : dinnerParties) {
             System.out.println(dinnerParty);
         }
-
     }
-
 }
